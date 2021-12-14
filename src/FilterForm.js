43724ea5f68cc26e274cc5415ce275229f5d7196
woodwardem <woodwardem@g.crookcountyschools.org>
2021-12-14 08:55:59 -0800

    added fuctionality to drop down menu

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
-
+import beastData from './beastData.json'
 class FilterForm extends React.Component {
+
+filter = (event) => {
+    console.log('event.target.value: ', event.target.value);
+    const numOfHorns = parseInt(event.target.value);
+    let gallery = beastData;
+    if (numOfHorns) {
+gallery = beastData.filter(beastObj => beastObj.horns === numOfHorns);
+    }
+}
+//TODO: Lift the new state of the gallery to it's parent component, to be rendered.
+
     render() {
         return (
             <>
                 <Form className="form">
                     <Form.Label for="numOfHorns">Filter by Horns</Form.Label>
                     <br />
-                    <Form.Select className="formSelect" name="numOfHorns">
+                    <Form.Select className="formSelect" name="numOfHorns" onChange={this.filter}>
 
 
-                        <option>ALL THE HORNS!</option>
-                        <option>One Horn</option>
-                        <option>Two Horns</option>
-                        <option>Three Horns</option>
-                        <option>More Horns Than I Can Count!</option>
+                        <option value=''>ALL THE HORNS!</option>
+                        <option value='1'>One Horn</option>
+                        <option value='2'>Two Horns</option>
+                        <option value='3'>Three Horns</option>
+                        <option value='100'>More Horns Than I Can Count!</option>
                     </Form.Select>
                 </Form>
 
