decc4ade699aa4507ffce64f049c4dc6bb5faee1
woodwardem <woodwardem@g.crookcountyschools.org>
2022-01-02 12:11:58 -0800

    fixing errors for deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
+
 import React from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import Card from 'react-bootstrap/Card';
+
 
 class App extends React.Component {
   render() {
